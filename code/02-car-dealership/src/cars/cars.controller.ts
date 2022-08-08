195945f95ef7a11460be684e49fea68742e479b3
Christopher Flores <furbox@gmail.com>
2022-08-08 17:00:47 -0500

    end seccion 4

--- code/02-car-dealership/src/cars/cars.controller.ts
+++ code/02-car-dealership/src/cars/cars.controller.ts
@@ -1,6 +1,6 @@
 import { Body, Controller, Delete, Get, Param, ParseUUIDPipe, Patch, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { CarsService } from './cars.service';
-import { CreateCarDto } from './dto/create-car.dto';
+import { CreateCarDto, UpdateCarDto } from './dto';
 
 @Controller('cars')
 export class CarsController {
@@ -17,19 +17,16 @@ export class CarsController {
 
   @Post()
   createCar(@Body() createCarDto: CreateCarDto) {
-    return createCarDto;
+    return this.carsService.create(createCarDto);
   }
 
   @Patch(':id')
-  updateCar(@Param('id', ParseUUIDPipe) id: string, @Body() body: any) {
-    return body;
+  updateCar(@Param('id', ParseUUIDPipe) id: string, @Body() updateCarDto: UpdateCarDto) {
+    return this.carsService.update(id, updateCarDto);
   }
 
   @Delete(':id')
   deleteCar(@Param('id', ParseUUIDPipe) id: string) {
-    return {
-      msg: 'delete',
-      id
-    };
+    return this.carsService.delete(id);
   }
 }
--- code/02-car-dealership/src/cars/cars.service.ts
+++ code/02-car-dealership/src/cars/cars.service.ts
@@ -1,5 +1,6 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
-import {v4 as uuid} from 'uuid';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
+import { v4 as uuid } from 'uuid';
+import { CreateCarDto, UpdateCarDto } from './dto';
 import { Car } from './interfaces/car.interface';
 
 @Injectable()
@@ -35,5 +36,40 @@ export class CarsService {
     const car = this.cars.find((car) => car.id === id);
     if (!car) throw new NotFoundException(`Car with id ${id} not found`);
     return car;
+  }
+
+  create(createCarDto: CreateCarDto) {
+    const car: Car = {
+      id: uuid(),
+      ...createCarDto
+    }
+    this.cars.push(car);
+    return car;
+  }
+
+  update(id: string, updateCarDto: UpdateCarDto) {
+    let carDB = this.findOneById(id);
+    if (updateCarDto.id && updateCarDto.id !== id) throw new BadRequestException(`invalid id ${updateCarDto.id}`);
+    this.cars = this.cars.map(car => {
+      if (car.id === id) {
+        carDB = {
+          ...carDB,
+          ...updateCarDto,
+          id
+        }
+        return carDB;
+      }
+      return car;
+    })
+    return carDB;
+  }
+
+  delete(id: string) {
+    const car = this.findOneById(id);
+    this.cars.filter(car => car.id !== id);
+    return {
+      ok: true,
+      msg: 'delete success'
+    }
   }
 }
